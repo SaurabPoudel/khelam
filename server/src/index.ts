d0a290d07f56a9343a6a14d9829f132f2a929bac
SaurabPoudel <poudelsaurab20@gmail.com>
2023-12-05 22:17:34 +0545

    server init

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,21 @@
-import { Express } from "express";
+import express,{ Express, Request , Response} from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app: Express = express();
+
+app.get('/',(req: Request, res: Response)=>{
+  res.send("game room server");
+})
+
+app.listen(port,()=>{
+  console.log(`😝[server]: Server is running at http://localhost:${port}`)
+})
+
+
+
+
 
